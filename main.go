2705e78b8fca1c5d95ff48c1bc882d39b63a101d
Distortions <33408758+Distortions81@users.noreply.github.com>
2020-07-16 00:02:19 -0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	service := ":7777"
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	defer listener.Close()
+
+	listenForConnections(listener)
+}
+
+func listenForConnections(listener *net.TCPListener) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		newDescriptor(conn)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func newDescriptor(connection net.Conn) {
+	connection.Write([]byte("testing 123...\n"))
+}
