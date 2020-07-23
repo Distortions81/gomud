fa89a241d74212f8c840110e3091a2fe16dd13f4
Distortions81 <carlotto81@gmail.com>
2020-07-23 15:55:24 -0600

    inital

---
+++ support/ansi-code.go
@@ -0,0 +1,13 @@
+package support
+
+import (
+	"github.com/Distortions81/ansi"
+)
+
+func ProcessColorCodes() {
+	phosphorize := ansi.ColorFunc("green+h:black")
+
+	if phosphorize != nil {
+		//
+	}
+}
